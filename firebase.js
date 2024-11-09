f46502199578aa7dc63c372f9ebe15c0ef4a0e6a
Ieen <106784528+Davizao01@users.noreply.github.com>
2024-11-09 20:50:03 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,41 +1,42 @@
-// firebase.js
-
-// Sua configuração do Firebase
+// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBiZLQyXq4yCRbX3k7A3K5j5fA2r9JO2VE",
-  authDomain: "seu-projeto.firebaseapp.com",
-  databaseURL: "https://seu-projeto.firebaseio.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: 'AIzaSyBiZLQyXq4yCRbX3k7A3K5j5fA2r9JO2VE',
+  authDomain: 'speedbank-10cda.firebaseapp.com',
+  projectId: 'speedbank-10cda',
+  storageBucket: 'speedbank-10cda.appspot.com',
+  messagingSenderId: '199708705722',
+  appId: '1:199708705722:web:4e9b9280c5b60e8c14f734',
+  measurementId: 'G-2ZQ7LZK29F'
 };
 
+// Inicializar o Firebase
 firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.database();
-
+// Funções do Firebase
 export function registerUser(email, password) {
-  return auth.createUserWithEmailAndPassword(email, password);
+  return firebase.auth().createUserWithEmailAndPassword(email, password);
 }
 
 export function loginUser(email, password) {
-  return auth.signInWithEmailAndPassword(email, password);
+  return firebase.auth().signInWithEmailAndPassword(email, password);
 }
 
 export function logoutUser() {
-  return auth.signOut();
+  return firebase.auth().signOut();
 }
 
 export function saveRun(user, score) {
-  return db.ref('runs').push({
+  const db = firebase.database();
+  const runRef = db.ref('runs').push();
+  return runRef.set({
     user: user,
     score: score,
-    status: 'Pendente'
+    status: 'Pendente',
   });
 }
 
 export function getRuns() {
-  return db.ref('runs').once('value');
+  const db = firebase.database();
+  const runsRef = db.ref('runs');
+  return runsRef.once('value');
 }
