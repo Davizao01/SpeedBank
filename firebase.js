ec46a6f1a6f2c711d0c7ad2d3f22ec41a5f73d3c
Ieen <106784528+Davizao01@users.noreply.github.com>
2024-11-09 21:28:22 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,15 @@
-// Configuração do Firebase
+// firebase.js
+
+// Sua configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBiZLQyXq4yCRbX3k7A3K5j5fA2r9JO2VE",
-  authDomain: "speedbank-10cda.firebaseapp.com",
-  projectId: "speedbank-10cda",
-  storageBucket: "speedbank-10cda.appspot.com",
-  messagingSenderId: "199708705722",
-  appId: "1:199708705722:web:4e9b9280c5b60e8c14f734",
-  measurementId: "G-2ZQ7LZK29F"
+  apiKey: 'AIzaSyBiZLQyXq4yCRbX3k7A3K5j5fA2r9JO2VE',
+  authDomain: 'speedbank-10cda.firebaseapp.com',
+  projectId: 'speedbank-10cda',
+  storageBucket: 'speedbank-10cda.appspot.com',
+  messagingSenderId: '199708705722',
+  appId: '1:199708705722:web:4e9b9280c5b60e8c14f734',
+  measurementId: 'G-2ZQ7LZK29F'
 };
 
+// Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);  // Garante que a sessão será persistida mesmo após fechar o navegador
