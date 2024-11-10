713522b6e26636cbccaaf320f5867d6ef2b73d9b
Ieen <106784528+Davizao01@users.noreply.github.com>
2024-11-09 21:03:11 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,42 +1,21 @@
-// Configuração do Firebase
-const firebaseConfig = {
-  apiKey: 'AIzaSyBiZLQyXq4yCRbX3k7A3K5j5fA2r9JO2VE',
-  authDomain: 'speedbank-10cda.firebaseapp.com',
-  projectId: 'speedbank-10cda',
-  storageBucket: 'speedbank-10cda.appspot.com',
-  messagingSenderId: '199708705722',
-  appId: '1:199708705722:web:4e9b9280c5b60e8c14f734',
-  measurementId: 'G-2ZQ7LZK29F'
-};
+<!-- Firebase (versão 8.x) -->
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
 
-// Inicializar o Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Funções do Firebase
-export function registerUser(email, password) {
-  return firebase.auth().createUserWithEmailAndPassword(email, password);
-}
-
-export function loginUser(email, password) {
-  return firebase.auth().signInWithEmailAndPassword(email, password);
-}
-
-export function logoutUser() {
-  return firebase.auth().signOut();
-}
-
-export function saveRun(user, score) {
-  const db = firebase.database();
-  const runRef = db.ref('runs').push();
-  return runRef.set({
-    user: user,
-    score: score,
-    status: 'Pendente',
-  });
-}
-
-export function getRuns() {
-  const db = firebase.database();
-  const runsRef = db.ref('runs');
-  return runsRef.once('value');
-}
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyBiZLQyXq4yCRbX3k7A3K5j5fA2r9JO2VE",
+    authDomain: "speedbank-10cda.firebaseapp.com",
+    projectId: "speedbank-10cda",
+    storageBucket: "speedbank-10cda.appspot.com",
+    messagingSenderId: "199708705722",
+    appId: "1:199708705722:web:4e9b9280c5b60e8c14f734",
+    measurementId: "G-2ZQ7LZK29F"
+  };
+  
+  // Inicializando o Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const database = firebase.database();
+</script>
